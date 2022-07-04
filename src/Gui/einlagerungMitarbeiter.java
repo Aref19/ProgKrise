f99d2015d90ca8acc82c3c13bf05476b035cd3d4
Aj <Ajabnoorkhan735@gmail.com>
2022-07-04 18:24:13 +0200

    GUI

---
+++ src/Gui/einlagerungMitarbeiter.java
@@ -0,0 +1,12 @@
+package Gui;
+
+import javax.swing.*;
+
+public class einlagerungMitarbeiter {
+    private JPanel ArtikelRootPanel;
+    private JLabel artikelHinzufuegen;
+    private JTextField artikelHinzufuegenTextField;
+    private JTextField BestandArtikelTextField;
+    private JButton artikelEinfuegenbutton;
+    private JButton zuruckZuMenu;
+}
---
+++ src/Gui/mainMitarbeiter.java
@@ -0,0 +1,18 @@
+package Gui;
+
+import javax.swing.*;
+
+public class mainMitarbeiter extends JFrame {
+    private JPanel rootPanel;
+    private JPanel mitarbeiterPanel;
+    private JTextField vornameTextFeld;
+    private JLabel nachnameJLabel;
+    private JTextField nachnameTextField;
+    private JButton neuMitarbeiterRegistrieren;
+    private JButton mainMeanu;
+    private JLabel passwort;
+    private JTextField textField1;
+
+
+
+}
